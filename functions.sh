f059187e713a553408dddf80f7db477f7f1ca5d2
Marek Zielonkowski <apsikz@gmail.com>
2017-07-27 19:47:27 +0200

    locs() function for counting JavaScript lines of code in current dir

--- functions.sh
+++ functions.sh
@@ -3,3 +3,8 @@
 lso() {
 ls -alG "$@" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print}';
 }
+
+locs() {
+find . -name '*.js' | xargs -L 1 -I'{}' cat {} | wc -l | tr '\n' ' ';
+echo 'locs of JavaScript code';
+}
