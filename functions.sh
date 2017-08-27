0e1b6a92f31607e786b304ddd330d9951503a934
Marek Zielonkowski <apsikz@gmail.com>
2017-08-27 22:21:04 +0200

    lso --group-directories-first

--- functions.sh
+++ functions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lso() {
-ls -alG "$@" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print}';
+ls -alG --group-directories-first "$@" | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(" %0o ",k);print}';
 }
 
 locs() {
