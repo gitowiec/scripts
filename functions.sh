535c7a412b4d879fb3003c9b8346cc7f89257a4e
Marek Zielonkowski <marek.zielonkowski@jbsolutions.pl>
2019-06-14 16:41:10 +0200

    docker-ssh()

--- functions.sh
+++ functions.sh
@@ -122,3 +122,16 @@ aptGetCleanLists() {
     sudo apt-get update
     popd
 }
+
+# https://askubuntu.com/a/759459/28081
+docker-ssh() {
+    container=$@
+    if [[ "$container" == "" ]]; then
+        # if no id given simply just connect to the first running container
+        container=$(docker ps | grep -Eo "^[0-9a-z]{8,}\b")
+    fi
+    # start an interactive bash inside the container
+    # note some containers don't have bash, then try: ash (alpine), or simply sh
+    # the -l at the end stands for login shell that reads profile files (read man)
+    docker exec -i -t $container bash -l
+}
